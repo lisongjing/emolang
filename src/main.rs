a6b9ff277180c16371fd62ef020e4385dcd1b67d
lisongjing <songjingcode@gmail.com>
2026-01-15 13:35:04 +0800

    add REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
+use emolang::repl;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello {}, Welcome to EMO programming language!", get_user_name());
+    println!("Feel free to start coding ⌨️");
+    repl::start();
+}
+
+fn get_user_name() -> String {
+    for envname in ["USER", "USERNAME", "LOGNAME"] {
+        if let Ok(username) = env::var(envname) && !username.is_empty() {
+            return username;
+        }
+    }
+    String::from("🧑🏻‍💻")
 }
---
+++ src/repl.rs
@@ -0,0 +1,16 @@
+use std::io::{self, Write};
+
+use crate::lexer::Lexer;
+
+
+pub fn start() {
+    loop {
+        print!(">> ");
+        io::stdout().flush().expect("Cannot write to console output");
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).expect("Cannot read from console input");
+        for token in Lexer::new(line).tokenize() {
+            println!("{token:?}");
+        }
+    }
+}
