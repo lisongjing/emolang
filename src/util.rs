c7a2b88732e582e691b9d62b748029f713b15e86
lisongjing <songjingcode@gmail.com>
2026-01-21 21:33:51 +0800

    add util

---
+++ src/util.rs
@@ -0,0 +1,115 @@
+use std::sync::mpsc::Iter;
+
+pub struct StatefulVector<T: PartialEq> {
+    vector: Vec<T>,
+    position: usize,
+}
+
+impl<T: PartialEq> StatefulVector<T> {
+    pub fn new() -> StatefulVector<T> {
+        StatefulVector {
+            vector: vec![],
+            position: 0,
+        }
+    }
+
+    pub fn from_vec(vector: Vec<T>) -> StatefulVector<T> {
+        StatefulVector {
+            vector,
+            position: 0,
+        }
+    }
+
+    pub fn from_iter(iterator: Iter<T>) -> StatefulVector<T> {
+        StatefulVector {
+            vector: iterator.collect::<Vec<T>>(),
+            position: 0,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.vector.is_empty()
+    }
+
+    pub fn first(&self) -> Option<&T> {
+        self.vector.first()
+    }
+
+    pub fn last(&self) -> Option<&T> {
+        self.vector.last()
+    }
+
+    pub fn current(&self) -> Option<&T> {
+        if self.is_pos_valid(self.position) {
+            Some(&self.vector[self.position])
+        } else {
+            None
+        }
+    }
+
+    pub fn next(&mut self) -> Option<&T> {
+        if self.has_next() {
+            self.position += 1;
+            Some(&self.vector[self.position])
+        } else {
+            None
+        }
+    }
+
+    pub fn previous(&mut self) -> Option<&T> {
+        if self.has_previous() {
+            self.position -= 1;
+            Some(&self.vector[self.position])
+        } else {
+            None
+        }
+    }
+
+    pub fn is_next_eq(&self, expected_element: &T) -> bool {
+        self.is_next_match(|ele| *ele == *expected_element)
+    }
+
+    pub fn is_next_match(&self, predicate: impl FnOnce(&T) -> bool) -> bool {
+        if self.has_next() {
+            predicate(&self.vector[self.position + 1])
+        } else {
+            false
+        }
+    }
+
+    pub fn push(&mut self, element: T) {
+        self.vector.push(element);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        let ele = self.vector.pop();
+        if ele.is_some() && self.position >= self.vector.len() {
+            self.position = self.vector.len() - 1
+        };
+        ele
+    }
+
+    pub fn has_next(&self) -> bool {
+        self.is_pos_valid(self.position + 1)
+    }
+
+    pub fn has_previous(&self) -> bool {
+        self.is_pos_valid(self.position - 1)
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<'_, T> {
+        self.vector.iter()
+    }
+
+    pub fn to_vec(self) -> Vec<T> {
+        self.vector
+    }
+
+    fn is_pos_valid(&self, pos: usize) -> bool {
+        if self.is_empty() {
+            false
+        } else {
+            pos < self.vector.len()
+        }
+    }
+}
