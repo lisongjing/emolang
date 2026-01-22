f3366d7268377e6e026ac90ce44ad9391685b40f
lisongjing <songjingcode@gmail.com>
2026-01-22 13:21:12 +0800

    refactor parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 
-use crate::lexer::{Lexer, Token, TokenType};
+use crate::{
+    lexer::{Lexer, Token, TokenType},
+    util::StatefulVector,
+};
 
 pub enum Precedence {
     Lowest,
@@ -48,9 +51,17 @@ impl Node for AssignStatement {
     fn token_literal(&self) -> &str {
         &self.token.literal
     }
-    
+
     fn string(&self) -> String {
-        format!("{} {} {} ↙️", self.name.string(), self.token.literal, self.value.as_ref().map(|exp| exp.string()).unwrap_or_default())
+        format!(
+            "{} {} {} ↙️",
+            self.name.string(),
+            self.token.literal,
+            self.value
+                .as_ref()
+                .map(|exp| exp.string())
+                .unwrap_or_default()
+        )
     }
 }
 
@@ -65,9 +76,16 @@ impl Node for ReturnStatement {
     fn token_literal(&self) -> &str {
         &self.token.literal
     }
-    
+
     fn string(&self) -> String {
-        format!("{} {} ↙️", self.token.literal, self.value.as_ref().map(|exp| exp.string()).unwrap_or_default())
+        format!(
+            "{} {} ↙️",
+            self.token.literal,
+            self.value
+                .as_ref()
+                .map(|exp| exp.string())
+                .unwrap_or_default()
+        )
     }
 }
 
@@ -75,16 +93,16 @@ impl Statement for ReturnStatement {}
 
 pub struct ExpressionStatement {
     token: Token,
-    expression: Option<Box<dyn Expression>>,
+    expression: Box<dyn Expression>,
 }
 
 impl Node for ExpressionStatement {
     fn token_literal(&self) -> &str {
         &self.token.literal
     }
-    
+
     fn string(&self) -> String {
-        self.expression.as_ref().map(|exp| exp.string()).unwrap_or_default()
+        self.expression.string()
     }
 }
 
@@ -99,7 +117,7 @@ impl Node for Identifier {
     fn token_literal(&self) -> &str {
         &self.token.literal
     }
-    
+
     fn string(&self) -> String {
         self.value.clone()
     }
@@ -107,108 +125,125 @@ impl Node for Identifier {
 
 impl Expression for Identifier {}
 
-type PrefixParser = Box<dyn Fn(&[Token], &mut usize) -> Box<dyn Expression>>;
-type InfixParser = Box<dyn Fn(&[Token], &mut usize, Box<dyn Expression>) -> Box<dyn Expression>>;
+type PrefixParser = Box<dyn Fn(&Parser) -> Box<dyn Expression>>;
+type InfixParser = Box<dyn Fn(&Parser, Box<dyn Expression>) -> Box<dyn Expression>>;
 
 pub struct Parser {
-    lexer: Lexer,
+    tokens: StatefulVector<Token>,
     errors: Vec<String>,
     prefix_exp_parsers: HashMap<TokenType, PrefixParser>,
     infix_exp_parsers: HashMap<TokenType, InfixParser>,
 }
 
 impl Parser {
-    pub fn new(lexer: Lexer) -> Parser {
-        let mut prefix_exp_parsers = HashMap::new();
-        let identifier_parser: PrefixParser = Box::new(parse_identifier);
-        prefix_exp_parsers.insert(TokenType::Identifier, identifier_parser);
+    pub fn new(lexer: &mut Lexer<'_>) -> Parser {
+        let prefix_exp_parsers = HashMap::new();
+        let infix_exp_parsers = HashMap::new();
+        let mut parser = Parser {
+            tokens: lexer.tokenize(),
+            errors: vec![],
+            prefix_exp_parsers,
+            infix_exp_parsers,
+        };
 
-        Parser { lexer, errors: vec![], prefix_exp_parsers, infix_exp_parsers: HashMap::new() }
+        parser
+            .prefix_exp_parsers
+            .insert(TokenType::Identifier, Box::new(|p| p.parse_identifier()));
+
+        parser
     }
 
     pub fn parse_program(&mut self) -> Program {
-        let mut program = Program {
-            statements: vec![],
-        };
-        let tokens = self.lexer.tokenize();
-        let mut pos = 0usize;
+        let mut program = Program { statements: vec![] };
 
-        while pos < tokens.len() {
-            let next_token = if pos < tokens.len() + 1 {
-                &tokens[pos + 1]
-            } else {
-                &Token::end()
-            };
-
-            let statement = match &tokens[pos].token_type {
-                TokenType::Identifier if is_token_type(TokenType::Assign, next_token) => {
-                    parse_assign_statement(&tokens, &mut pos)
-                }
-                TokenType::Return => parse_return_statement(&tokens, &mut pos),
-                _ => parse_expression_statement(&tokens, &mut pos),
+        while let Some(token) = self.tokens.to_next() {
+            let statement = match token.token_type {
+                TokenType::Identifier => self.parse_assign_statement(),
+                TokenType::Return => self.parse_return_statement(),
+                _ => self.parse_expression_statement(),
             };
             match statement {
                 Ok(statement) => program.statements.push(statement),
                 Err(error_msg) => self.errors.push(error_msg),
             }
-            pos += 1;
         }
 
         program
     }
-}
 
-fn is_token_type(expected_token_type: TokenType, token: &Token) -> bool {
-    expected_token_type == token.token_type
-}
+    fn parse_assign_statement(&mut self) -> Result<Box<dyn Statement>, String> {
+        if self
+            .tokens
+            .is_next_match(|tok| tok.token_type != TokenType::Assign)
+        {
+            return self.parse_expression_statement();
+        }
 
-fn parse_assign_statement(tokens: &[Token], pos: &mut usize) -> Result<Box<dyn Statement>, String> {
-    let identifier = tokens[*pos].clone();
-    let identifier = Identifier {
-        token: identifier.clone(),
-        value: identifier.literal,
-    };
-    *pos += 1;
-    let assign_token = tokens[*pos].clone();
-    while tokens[*pos].token_type != TokenType::Semicolon {
-        *pos += 1;
-    }
-    Ok(Box::new(AssignStatement {
-        token: assign_token,
-        name: identifier,
-        value: None,
-    }))
-}
-
-fn parse_return_statement(tokens: &[Token], pos: &mut usize) -> Result<Box<dyn Statement>, String> {
-    let return_token = tokens[*pos].clone();
-    *pos += 1;
-    while tokens[*pos].token_type != TokenType::Semicolon {
-        *pos += 1;
-    }
-    Ok(Box::new(ReturnStatement {
-        token: return_token,
-        value: None,
-    }))
-}
-
-fn parse_expression_statement(tokens: &[Token], pos: &mut usize) -> Result<Box<dyn Statement>, String> {
-    let exp_token = tokens[*pos].clone();
-
-    if *pos < tokens.len() - 1 && is_token_type(TokenType::Semicolon, &tokens[*pos + 1]) {
-        *pos += 1;
+        let identifier = self.tokens.current().unwrap().clone();
+        let identifier = Identifier {
+            token: identifier.clone(),
+            value: identifier.literal,
+        };
+        let assign_token = self.tokens.to_next().unwrap().clone();
+        while let Some(token) = self.tokens.to_next()
+            && token.token_type != TokenType::Semicolon
+        {
+            &token.literal;
+        }
+        Ok(Box::new(AssignStatement {
+            token: assign_token,
+            name: identifier,
+            value: None,
+        }))
     }
 
-    Ok(Box::new(ExpressionStatement {
-        token: exp_token,
-        expression: None,
-    }))
-}
+    fn parse_return_statement(&mut self) -> Result<Box<dyn Statement>, String> {
+        let return_token = self.tokens.current().unwrap().clone();
+        while let Some(token) = self.tokens.to_next()
+            && token.token_type != TokenType::Semicolon
+        {
+            &token.literal;
+        }
+        Ok(Box::new(ReturnStatement {
+            token: return_token,
+            value: None,
+        }))
+    }
 
-fn parse_identifier(tokens: &[Token], pos: &mut usize) -> Box<dyn Expression> {
-    Box::new(Identifier { token: tokens[*pos].clone(), value: tokens[*pos].literal.clone() })
-}
+    fn parse_expression_statement(&mut self) -> Result<Box<dyn Statement>, String> {
+        let exp_token = self.tokens.current().unwrap().clone();
+        let exp = self.parse_expression(Precedence::Lowest)?;
 
+        if self
+            .tokens
+            .is_next_match(|tok| tok.token_type == TokenType::Semicolon)
+        {
+            self.tokens.to_next();
+        }
+
+        Ok(Box::new(ExpressionStatement {
+            token: exp_token,
+            expression: exp,
+        }))
+    }
+
+    fn parse_expression(&self, precedence: Precedence) -> Result<Box<dyn Expression>, String> {
+        let token = self.tokens.current().unwrap();
+
+        self.prefix_exp_parsers
+            .get(&token.token_type)
+            .map(|f| f(self))
+            .ok_or_else(|| format!("Expected a expression, but got a {}", token.literal))
+    }
+
+    fn parse_identifier(&self) -> Box<dyn Expression> {
+        let token = self.tokens.current().unwrap().clone();
+        Box::new(Identifier {
+            token: token.clone(),
+            value: token.literal.clone(),
+        })
+    }
+}
 
 #[cfg(test)]
 mod parser_test {
@@ -224,8 +259,8 @@ mod parser_test {
         ",
         );
 
-        let lexer = Lexer::new(source);
-        let mut parser = Parser::new(lexer);
+        let mut lexer = Lexer::new(&source);
+        let mut parser = Parser::new(&mut lexer);
         let program = parser.parse_program();
 
         assert_eq!(program.statements.len(), 2);
