36fab3f41997ed0442a52289ca87526af7dac476
lisongjing <songjingcode@gmail.com>
2026-02-06 12:06:53 +0800

    refactor object types

--- src/types/node.rs
+++ src/types/node.rs
@@ -1,5 +1,3 @@
-use std::fmt::Debug;
-
 use crate::types::{Token, TokenType};
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
--- src/types/object.rs
+++ src/types/object.rs
@@ -1,80 +1,19 @@
-pub enum ObjectType {
-    Integer,
-    Float,
-    Boolean,
-    String,
+pub enum Object {
+    Integer(i64),
+    Float(f64),
+    Boolean(bool),
+    String(String),
     Null,
 }
 
-pub trait Object {
-    fn object_type(&self) -> ObjectType;
-    fn inspect(&self) -> String;
-}
-
-pub struct Integer {
-    value: i64,
-}
-
-impl Object for Integer {
-    fn object_type(&self) -> ObjectType {
-        ObjectType::Integer
-    }
-
-    fn inspect(&self) -> String {
-        self.value.to_string()
-    }
-}
-
-pub struct Float {
-    value: f64,
-}
-
-impl Object for Float {
-    fn object_type(&self) -> ObjectType {
-        ObjectType::Float
-    }
-
-    fn inspect(&self) -> String {
-        self.value.to_string()
-    }
-}
-
-pub struct Boolean {
-    value: bool,
-}
-
-impl Object for Boolean {
-    fn object_type(&self) -> ObjectType {
-        ObjectType::Boolean
-    }
-
-    fn inspect(&self) -> String {
-        self.value.to_string()
-    }
-}
-
-pub struct MutableString {
-    value: String,
-}
-
-impl Object for MutableString {
-    fn object_type(&self) -> ObjectType {
-        ObjectType::String
-    }
-
-    fn inspect(&self) -> String {
-        self.value.clone()
-    }
-}
-
-pub struct EmoNull {}
-
-impl Object for EmoNull {
-    fn object_type(&self) -> ObjectType {
-        ObjectType::Null
-    }
-
-    fn inspect(&self) -> String {
-        "null".to_string()
+impl Object {
+    pub fn inspect(&self) -> String {
+        match self {
+            Object::Integer(value) => value.to_string(),
+            Object::Float(value) => value.to_string(),
+            Object::Boolean(value) => value.to_string(),
+            Object::String(value) => value.clone(),
+            Object::Null => "null".to_string(),
+        }
     }
 }
