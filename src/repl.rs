f47b6b7ea860dd73dabad80047c5b228d936dece
songjing <songjingcode@gmail.com>
2026-02-10 15:34:13 +0800

    update repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,22 +1,29 @@
 use std::io::{self, Write};
 
-use crate::{lexer::Lexer, parser::Parser};
-
+use crate::{evaluator::eval, lexer::Lexer, parser::Parser};
 
 pub fn start() {
     loop {
         print!(">> ");
-        io::stdout().flush().expect("Cannot write to console output");
+        io::stdout()
+            .flush()
+            .expect("Cannot write to console output");
         let mut line = String::new();
-        io::stdin().read_line(&mut line).expect("Cannot read from console input");
+        io::stdin()
+            .read_line(&mut line)
+            .expect("Cannot read from console input");
         let mut lexer = Lexer::new(&line);
-        let mut parser= Parser::new(&mut lexer);
+        let mut parser = Parser::new(&mut lexer);
         let program = parser.parse_program();
 
         if !parser.errors().is_empty() {
-            println!("Paser errors:\n{}", parser.errors().join("\n"));
+            println!("Paser errors:\n\t{}", parser.errors().join("\n\t"));
             continue;
         }
-        println!("{}", program.string());
+
+        match eval(program) {
+            Ok(evaluated) => println!("{}", evaluated.inspect()),
+            Err(error) => println!("Evaluator error:\n\t{error}"),
+        }
     }
 }
