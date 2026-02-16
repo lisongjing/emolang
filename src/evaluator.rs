738cae212655be42e184ae5ef364f964ec487740
songjing <songjingcode@gmail.com>
2026-02-16 14:50:41 +0800

    fix return expression evaluation

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -2,7 +2,7 @@ use crate::types::{Node, object::*};
 
 pub fn eval(node: Node) -> Result<Object, String> {
     match node {
-        Node::Program { statements } => eval_statements(statements),
+        Node::Program { statements } => eval_program(statements),
         Node::ExpressionStatement { token: _, expression } => eval(*expression),
         Node::IntegerLiteral { token: _, value } => Ok(Object::Integer(value)),
         Node::FloatLiteral { token: _, value } => Ok(Object::Float(value)),
@@ -10,23 +10,33 @@ pub fn eval(node: Node) -> Result<Object, String> {
         Node::StringLiteral { token: _, value } => Ok(Object::String(value)),
         Node::PrefixExpression { token: _, operator, right } => eval_prefix_expression(operator, eval(*right)?),
         Node::InfixExpression { token: _, left, operator, right } => eval_infix_expression(operator, eval(*left)?, eval(*right)?),
-        Node::BlockStatement { token: _, statements } => eval_statements(statements),
+        Node::BlockStatement { token: _, statements } => eval_block_statements(statements),
         Node::IfExpression { token: _, condition, consequence, alternative } => eval_if_expression(condition, consequence, alternative),
         Node::ReturnStatement { token: _, value } => Ok(Object::ReturnValue(Box::new(eval(*value)?))),
         _ => Err(String::from("Invalid expressions or statements to evaluate values"))
     }
 }
 
-fn eval_statements(statements: Vec<Node>) -> Result<Object, String> {
+fn eval_program(statements: Vec<Node>) -> Result<Object, String> {
     let mut result = Err(String::from("Empty statements to evaluate values"));
     for statement in statements {
         result = eval(statement);
+        if let Ok(Object::ReturnValue(value)) = result {
+            return Ok(*value);
+        }
     }
-    if let Ok(Object::ReturnValue(value)) = result {
-        Ok(*value)
-    } else {
-        result
+    result
+}
+
+fn eval_block_statements(statements: Vec<Node>) -> Result<Object, String> {
+    let mut result = Err(String::from("Empty statements to evaluate values"));
+    for statement in statements {
+        result = eval(statement);
+        if let Ok(Object::ReturnValue(_)) = &result {
+            return result;
+        }
     }
+    result
 }
 
 fn eval_prefix_expression(operator: String, right: Object) -> Result<Object, String> {
@@ -38,14 +48,16 @@ fn eval_prefix_expression(operator: String, right: Object) -> Result<Object, Str
 }
 
 fn eval_prefix_not_expression(obj: &Object) -> Result<Object, String> {
+    if let Object::ReturnValue(_) = &obj {
+        return Err(String::from("Invalid prefix not expression to evaluate return expression"));
+    }
     let value = match obj {
         Object::Integer(value) => *value > 0 ,
         Object::Float(value) => *value > 0.0,
         Object::Boolean(value) => *value,
         Object::String(value) => !value.is_empty(),
         Object::Null => false,
-        // todo return error
-        Object::ReturnValue(value) => false,
+        _ => false,
     };
     Ok(to_bool_object(!value))
 }
@@ -54,7 +66,7 @@ fn eval_prefix_minus_expression(obj: &Object) -> Result<Object, String> {
     match obj {
         Object::Integer(value) => Ok(Object::Integer(-value)) ,
         Object::Float(value) => Ok(Object::Float(-value)) ,
-        _ => Err(String::from("Invalid prefix minus expressions to evaluate non-numeric values")),
+        _ => Err(String::from("Invalid prefix minus expression to evaluate non-numeric value")),
     }
 }
 
